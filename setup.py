22ec923a2e3d561c157aab4b188146c17acac386
Timothy Edmund Crosley <timothy.crosley@gmail.com>
2012-08-15 22:04:41 -0400

    Update description / product url

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from distutils.core import setup
 
 setup(name='GuiBuilder',
       version='0.1',
-      description='Python Distribution Utilities',
+      description='A drag and drop interface to create WebElement templates quickly and easily.',
       author='Timothy Crosley',
       author_email='timothy.crosley@gmail.com',
-      url='http://www.webelements.in/',
+      url='http://www.guibuilder.net/',
       download_url='https://github.com/timothycrosley/GuiBuilder/blob/master/dist/GuiBuilder-0.1.tar.gz?raw=true',
       license = "GNU GPLv2",
       scripts=['scripts/guiBuilder', 'scripts/webElementDocs'],
